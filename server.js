12e913cbdb2481cb820524a4a575f9fd429f3937
ziyani.ismael <ziyani.ismael@live.dk>
2023-03-23 11:33:32 +0100

    changes to layout and returned values

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
-//const express = require('express');
-//const fetch = require('node-fetch');
 import express from 'express';
 import fetch from 'node-fetch';
+
 const app = express();
 const port = 3000;
 
@@ -9,8 +8,8 @@ app.use(express.static('public'));
 
 app.get('/images', async (req, res) => {
   const api_key = 'f738f463be0067ac0fc99a3d346cde2a';
-  const tag = 'Batman'; // Change this to any tag you want to search for
-  const url = `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${api_key}&tags=${tag}&per_page=10&format=json&nojsoncallback=1`;
+  const tag = 'Marvel'; // Change this to any tag you want to search for
+  const url = `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${api_key}&tags=${tag}&per_page=12&format=json&nojsoncallback=1`;
   console.log("Trying this link: " + url);
 
   try {
@@ -18,7 +17,11 @@ app.get('/images', async (req, res) => {
     const data = await response.json();
     const photos = data.photos.photo;
     const urls = photos.map((photo) => {
-      return `https://live.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`;
+      return {
+        url: `https://live.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`,
+        title: photo.title,
+        caption: photo.owner
+      };
     });
     res.send(urls);
   } catch (error) {
